3dbbf5fd9054394cdbc6bcaf6f2c407a507440f6
jiyeong0607 <kong24@ewha.ac.kr>
2025-06-08 17:36:03 +0900

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -10,10 +10,10 @@ function login() {
   .then(res => res.json())
   .then(data => {
     if (data.success) {
-      // ✅ studentId를 localStorage에 저장
+      // studentId를 localStorage에 저장
       localStorage.setItem("studentId", studentId);
 
-      // ✅ 캘린더 페이지로 이동
+      // 캘린더 페이지로 이동
       window.location.href = 'calendar.html';
     } else {
       document.getElementById("loginResult").innerText = "로그인 실패: 정보를 다시 확인하세요.";
